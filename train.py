d5cb2586ef043b084a043127e9a05664c9309de3
anna.khosrovyan <anna.khosrovyan.1806@gmail.com>
2025-09-29 10:45:48 +0400

    update seed configuration for reproducibility

--- train.py
+++ train.py
@@ -19,7 +19,8 @@ def set_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.benchmark = False
+    os.environ['PYTHONHASHSEED'] = str(seed)
 
 if __name__ == '__main__':
 
